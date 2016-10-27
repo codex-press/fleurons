661858942e9e8e7a779ec739c85a373d6b3e65c6
Tim Farnam <twfarnam@gmail.com>
2016-10-27 18:49:30 -0400

    adds compile function, optimizes

---
+++ compile.js
@@ -0,0 +1,54 @@
+#! /usr/local/bin/node
+
+var fs = require('fs');
+var SVGO = require('svgo');
+
+fs.readdirSync('./export').map(filename => {
+  if (/\.svg$/.test(filename)) {
+
+    var name = filename.slice(0,-4);
+
+    var xml = fs.readFileSync('./export/' + filename, {encoding: 'utf-8'});
+
+    var svgo = new SVGO({
+      plugins: [
+        {mergePaths: false},
+        {addClassesToSVGElement: {classNames: ['fleuron', 'f'+name]}},
+        {convertPathData: {
+          floatPrecision: 1,
+        }},
+        {addFleuronAttributes: {
+          type: 'full',
+          active: true,
+          fn: tree => {
+            let svg = tree.content[0];
+
+            if (svg.isElem('svg')) {
+              svg.attrs.width = {
+                name: 'width',
+                prefix: '',
+                value: '300',
+                local: ''
+              };
+
+              svg.attrs.style = {
+                name: 'style',
+                prefix: '',
+                value: 'display:block; margin: 1rem auto;',
+                local: ''
+              };
+            }
+
+            return tree;
+          }
+        }},
+      ]
+    });
+
+    svgo.optimize(xml, result => {
+      fs.writeFileSync(filename, result.data);
+    });
+
+  }
+});
+
